47c1b46c025c97129c557d55ca35316bf84a3daf
Szymon-Przybylski <51139859+Szymon-Przybylski@users.noreply.github.com>
2024-11-26 20:07:29 +0100

    updated identity service to work with prostgres db

--- src/IdentityService/SeedData.cs
+++ src/IdentityService/SeedData.cs
@@ -18,6 +18,12 @@ namespace IdentityService
                 context.Database.Migrate();
 
                 var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+                if (userMgr.Users.Any())
+                {
+                    return;
+                }
+
                 var alice = userMgr.FindByNameAsync("alice").Result;
                 if (alice == null)
                 {
@@ -35,9 +41,6 @@ namespace IdentityService
 
                     result = userMgr.AddClaimsAsync(alice, new Claim[]{
                                 new Claim(JwtClaimTypes.Name, "Alice Smith"),
-                                new Claim(JwtClaimTypes.GivenName, "Alice"),
-                                new Claim(JwtClaimTypes.FamilyName, "Smith"),
-                                new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
                             }).Result;
                     if (!result.Succeeded)
                     {
@@ -67,10 +70,6 @@ namespace IdentityService
 
                     result = userMgr.AddClaimsAsync(bob, new Claim[]{
                                 new Claim(JwtClaimTypes.Name, "Bob Smith"),
-                                new Claim(JwtClaimTypes.GivenName, "Bob"),
-                                new Claim(JwtClaimTypes.FamilyName, "Smith"),
-                                new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
-                                new Claim("location", "somewhere")
                             }).Result;
                     if (!result.Succeeded)
                     {
