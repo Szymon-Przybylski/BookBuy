faea890a25817f2278a453656b241b4004a04331
Szymon-Przybylski <51139859+Szymon-Przybylski@users.noreply.github.com>
2024-12-18 16:45:16 +0100

    added pagination

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,24 +1,40 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react';
 import AuctionCard from './AuctionCard';
-import { Auction, PagedResult } from '../types';
+import { Auction } from '../types';
+import AppPagination from '../components/AppPagination';
+import { getData } from '../actions/auctionActions';
+import Filters from './Filters';
 
-async function getData(): Promise<PagedResult<Auction>> {
-    const res = await fetch('http://localhost:6001/search');
 
-    if (!res.ok) throw new Error('failed to fetch data');
+export default function Listings() {
 
-    return res.json();
-}
+    const [auctions, setAuctions] = useState<Auction[]>([]);
+    const [pageCount, setPageCount] = useState(0);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [pageSize, setPageSize] = useState(2);
 
-export default async function Listings() {
+    useEffect(() => {
+        getData(pageNumber, pageSize).then( data => {
+            setAuctions(data.result);
+            setPageCount(data.pageCount)
+        })
+    }, [pageNumber, pageSize])
 
-    const data = await getData();
+    if(auctions.length === 0) return <h3>Loading...</h3>
 
     return (
-        <div className='grid grid-cols-4 gap-6'>
-            {data && data.result.map((auction) => (
-                <AuctionCard auction={auction} key={auction.id } />
-            ))}
-        </div>
+        <>
+            <Filters pageSize={pageSize} setPageSize={setPageSize}/>
+            <div className='grid grid-cols-4 gap-6'>
+                {auctions.map(auction => (
+                    <AuctionCard auction={auction} key={auction.id } />
+                ))}
+            </div>
+            <div className='flex justify-center mt-4'>
+                <AppPagination currentPage={pageNumber} pagecount={pageCount} pageChanged={setPageNumber}/>
+            </div>
+        </>
     )
 }
---
+++ frontend/web-app/app/components/AppPagination.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Pagination } from 'flowbite-react';
+import React from 'react';
+
+type Props = {
+    currentPage: number
+    pagecount: number
+    pageChanged: (page: number) => void
+}
+
+export default function AppPagination({currentPage, pagecount, pageChanged}: Props) {
+
+    return (
+        <Pagination 
+            currentPage={currentPage} 
+            onPageChange={e => pageChanged(e)} 
+            totalPages={pagecount} 
+            layout='pagination'
+            showIcons={true}
+            className='text-blue-500 mb-5'
+            />
+    )
+}
