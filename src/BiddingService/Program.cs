37a7429ef2a6007a957c892c7d5a27a6ba02a5b6
Szymon-Przybylski <51139859+Szymon-Przybylski@users.noreply.github.com>
2025-01-19 11:13:22 +0100

    added skeleton bidding service

---
+++ src/BiddingService/Program.cs
@@ -0,0 +1,53 @@
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("BiddingService", false));
+
+
+    x.UsingRabbitMq((context, configuration) =>
+    {
+        configuration.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
+        configuration.ConfigureEndpoints(context);
+    });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
+var environment = builder.Environment.EnvironmentName;
+Console.WriteLine($"Environment: {environment}");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await DB.InitAsync("BiddingServiceDatabase",
+ MongoClientSettings.FromConnectionString
+ (builder.Configuration.GetConnectionString("BiddingServiceDatabaseConnection")));
+
+app.Run();
